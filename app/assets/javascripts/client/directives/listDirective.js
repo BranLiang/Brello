4973936b7560f34dd446cf701cf465ea7966afcf
BranLiang <lby89757@hotmail.com>
2016-10-21 11:11:31 +0800

    fix the delete list function

--- app/assets/javascripts/client/directives/listDirective.js
+++ app/assets/javascripts/client/directives/listDirective.js
@@ -1,25 +1,26 @@
-Brello.directive("list", ['CardService', function(CardService){
-  return {
-    templateUrl: "templates/lists/_list.html",
-    restrict: "E",
-    scope: {
-      list: "="
-    },
-    link: function(scope){
-      // for the card service
-  		scope.createCard = function () {
-  			CardService.create({
-  				title: scope.cardParams.title,
-  				list_id: scope.list.id
-  			}).then(function(response){
-          scope.list.cards.push(response);
-        });
-  			scope.cardParams = {};
-  		};
+Brello.directive("list", ['CardService', function (CardService) {
+	return {
+		templateUrl: "templates/lists/_list.html",
+		restrict: "E",
+		scope: {
+			list: "=",
+			deleteList: "&"
+		},
+		link: function (scope) {
+			// for the card service
+			scope.createCard = function () {
+				CardService.create({
+					title: scope.cardParams.title,
+					list_id: scope.list.id
+				}).then(function (response) {
+					scope.list.cards.push(response);
+				});
+				scope.cardParams = {};
+			};
 
-      // scope.$on('card.created', function (event, card) {
-  		// 	scope.list.cards.push(card);
-  		// });
-    }
-  };
+			// scope.$on('card.created', function (event, card) {
+			// 	scope.list.cards.push(card);
+			// });
+		}
+	};
 }]);
